b2066920986fd23f294b2b7e30c54fd503f8f565
Eric Patrick <epatrick@quandis.com>
2019-01-11 10:05:59 -0500

    Adding sample import.

---
+++ Introduction.md
@@ -0,0 +1,27 @@
+# Power User Guide
+
+Quandis Business Objects (QBO) is a suite of software modules 
+intended to make it easy for businesses to automation business processes.
+Much as Microsoft Office integrated word processing, spreadsheets, slides 
+and more into an integrated suite of personal productivity tools, QBO
+offers workforce management, document management, analytics, accounting, 
+workflow and more as a suite of corporate productivity tools.
+
+## Overview 
+
+|Module|Description|
+|-|-|
+|Accounting|Create and management **invoices**, set up rules for automatic approval, rejections and reviews, credit card processing.|
+|Customer Relationship Management|Import or create companies and individuals, tracking messages, tasks, documents and accounting data.|
+|Data Management|Push and pull data from spreadsheets, flat files, XML, JSON, databases and web services.|
+|Document Management|Document imaging, mail merge, OCR, FTP, ZIP, and more.|
+|Messaging|Note taking, email and SMS texting, monitor email inboxes, canned messages and more.|
+|Process Management|Manage the end-to-end lifecycle of business processes, including outsourcing to third parties, launching workflows, tracking tasks, documents and accounting data.|
+|Rules Engine|Maintain **business rules** based on any inputs, including a weight-based inference engine and machine learning.|
+|Scoring|Create **analytic models** to evaluate your data, and use the results to drive workflow, merge documents, etc.|
+|Security|Manage users, roles, permissions, and the **extranet-based** security model that limits third party users to precisely the QBO data you need them to access.|
+|Workflow|Implement **flowcharts** that orchestrate data flows, user tasks, document creation and delivery, analytics and systems integration.|
+|Workforce Management|Manage user **tasks** with online **forms** that include **user-defined fields**, integrate data from multiple sources, automate **load balancing** assignment, and manage **productivity**. |
+
+## Accounting
+
---
+++ acme.Financial/src/server.Acme.Financial/PublicDataImport.cs
@@ -0,0 +1,80 @@
+﻿using qbo.Application.Interfaces;
+using qbo.Attachment;
+using qbo.Attachment.FileObjects;
+using qbo.Import.Configuration;
+using qbo.Import.Engines;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace server.Acme.Financial
+{
+    /// <summary>
+    /// Demonstrate importing an external data source into QBO.
+    /// </summary>
+    public class PublicDataImport : AbstractEngine
+    {
+        public PublicDataImport(ImportEngine configuration, IPerson user) : base(configuration, user)
+        {
+        }
+
+        public override bool Import(IDictionary<string, object> parameters)
+        {
+            return ImportAsync(parameters).GetAwaiter().GetResult();
+        }
+
+        public async override Task<bool> ImportAsync(IDictionary<string, object> parameters)
+        {
+            // Often, the source of data is picked up by ImportFile/Watch or a user dropping a file onto the Import dashboard.
+            // To keep this demo self-contained, we'll download a public spreadsheet from HUD and import that.
+            if (!ImportFile.PreTransformID.HasValue)
+            {
+                // Point the source of data to a public URL
+                ImportFile.PreTransform = new AttachmentObject(User)
+                {
+                    // Standard QBO installations include an Internet file object that allows reading URLs that are publically accessible.
+                    FileObjectAssembly = "Internet",
+                    PathURL = "https://www.huduser.gov/portal/datasets/fmr/fmr2019/FY19_4050_FMRs.xlsx"
+                };
+            }
+
+            // Many files, including Excel, need to be cached locally for processing.
+            // Attachment.CacheStream* methods copy a file to c:\windows\temp, and open a CacheFile over it.
+            // This temp file will be deleted on dispoal, so we don't clutter c:\windows\temp.
+            string scratchpad = AbstractFile.GetScratchpadFile();
+            using (var stream = new qbo.Attachment.FileObjects.CacheStream(scratchpad, System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.ReadWrite))
+            {
+                // Copy the data from the Internet to the scratchpad
+                await ImportFile.PreTransform.ReadAsync(stream);
+
+                // Prepare the stream for reading
+                stream.Position = 0;
+
+                // Set up an OldDbConnection to the Excel spreadsheet
+                var sourceConnection = $"Provider=Microsoft.ACE.OLEDB.12.0;Extended Properties=\"Excel 12.0;HRD=Yes;IMEX=1\";Data Source={scratchpad}";
+
+                // determine the connection string to the target SqlServer; in this case, we'll just used the default QBO database
+                var targetConnection = ConfigurationManager.ConnectionStrings["qbo.Default"];
+                using (OleDbConnection _Connection = new OleDbConnection(sourceConnection))
+                {
+                    // Wire a command to select data from the spreadsheet.
+                    // In this case, the spreadsheet from HUD has a worksheet called 'fmr19_info'. 
+                    var command = new OleDbCommand("SELECT * FROM [fmr19_info]", _Connection);
+                    using (var reader = await command.ExecuteReaderAsync())
+                    {
+                        SqlBulkCopy bulkCopy = new SqlBulkCopy(targetConnection.ConnectionString, SqlBulkCopyOptions.TableLock);
+                        bulkCopy.DestinationTableName = "[hud].[FairMarketRentValue]";
+                        bulkCopy.WriteToServer(reader);
+                    }
+                }
+
+            }
+
+            // Successful
+            return true;
+        }
+    }
+}
